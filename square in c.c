739db4e08c1b28fb3cb10e13593661eeb869fb57
gowdusudha <93200323+gowdusudha@users.noreply.github.com>
2021-11-03 11:32:27 +0530

    Add files via upload

---
+++ square in c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c;
+	a=2;
+	b=a*a;
+	c=b*a;
+	printf("area %d of a square with side is %d:", a, b );
+	printf("area %d and perimeter of a square with side is %d:", b, c);	
+}
