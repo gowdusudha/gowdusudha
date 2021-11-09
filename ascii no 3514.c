991a705fc98ac6e37e8dc588135ed340e5bad5d2
gowdusudha <93200323+gowdusudha@users.noreply.github.com>
2021-11-09 12:20:28 +0530

    Add files via upload

---
+++ ascii no 3514.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter a number (65-90 or 97-122):100");
+	scanf("%d",&n);
+	printf(" corresponding ASCII character %d is %c", n,n);
+	printf("%c%d,n,n");
+}
