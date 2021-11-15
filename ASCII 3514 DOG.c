6580261da04264666688a355224cb539e5d2bc68
gowdusudha <93200323+gowdusudha@users.noreply.github.com>
2021-11-15 11:49:54 +0530

    Add files via upload

---
+++ ASCII 3514 DOG.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter a character (65-90 or 97-122)");
+         scanf("%d%%d%d",&a,&b,&c);
+         printf("decode the three given  numbers");
+         printf("corresponding ASCII character for %d is %c",a,b,c);
+	
+}
