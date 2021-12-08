ecfcb8556ec04ed18f5b315f1e5a30607092fff9
gowdusudha <93200323+gowdusudha@users.noreply.github.com>
2021-12-08 11:35:53 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	scanf("%d",&n);
+	if (n%1==0)
+	{
+		printf("%d is divisible 1\n",n);
+	}
+	 if (n%2==0)
+	{
+		printf("%d is divisible 2\n",n);
+	}
+	if (n%3==0)
+	{
+		printf("%d is divisible 3\n",n);
+	}
+	 if(n%4==0)
+	{
+		printf("%d is divisible 4\n",n);
+	}
+	if(n%5==0)
+	{
+		printf("%d is divisible 5\n",n);
+	}
+     if(n%6==0)
+	{
+		printf("%d is divisible 6\n",n);
+	}
+	 if(n%7==0)
+	{
+		printf("%d is divisible 7\n",n);
+	}
+	if(n%8==0)
+	{
+		printf("%d is divisible 8\n",n);
+	}
+}
