274e4b58914702d5e0c7042978ed245f01602a20
gowdusudha <93200323+gowdusudha@users.noreply.github.com>
2021-12-08 12:17:28 +0530

    Add files via upload

--- pass o r fail.c
+++ pass o r fail.c
@@ -1,14 +1,38 @@
 #include<stdio.h>
 void main()
 { 
-  int a,b,c,d,e;
+  int a,b,c,d,e,total,perc;
   scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+  total=a+b+c+d+e;
+  perc=total/5;
   if(a>=35&&b>=35&&c>=35&&d>=35&&e>=35)
   {
-  	printf(" pass");
-  }
-  else
-  {
-  	printf("fail");
-  }
+  	printf(" pass\n");
+  	printf("total marks out of %d is %d\n",5000,total);
+  	printf("Grade: ");
+    if(perc>=90)
+    {
+    	printf("o");
+	}
+	else if (perc>=80)
+	{
+		printf("A");
+	}
+	else if (perc>=70)
+	{
+		printf("B");
+	}
+	else if (perc>=60)
+	{
+		printf("C");
+	}
+	else if (perc>=50)
+	{
+		printf("D");
+	}
+  	else
+  	{
+        printf("fail");
+	}
+   }
 }
